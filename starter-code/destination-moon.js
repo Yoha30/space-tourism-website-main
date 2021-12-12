fe5245d7a8e5f7b506c55c165f0ad246094628c4
Yoha30 <3202114888@alexu.edu.eg>
2021-12-12 02:26:37 +0200

    home-page

--- starter-code/destination-moon.js
+++ starter-code/destination-moon.js
@@ -1,16 +1,16 @@
-document.getElementById('list').addEventListener('mouseover',function () {
+document.getElementById('list').addEventListener('mouseover', function () {
     document.getElementById('line').classList.remove('hidden');
 });
 document.getElementById('list').addEventListener('mouseout', function () {
     document.getElementById('line').classList.add('hidden');
 });
-document.getElementById('list-2').addEventListener('mouseover',function () {
+document.getElementById('list-2').addEventListener('mouseover', function () {
     document.getElementById('line-2').classList.remove('hidden');
 });
 document.getElementById('list-2').addEventListener('mouseout', function () {
     document.getElementById('line-2').classList.add('hidden');
 });
-document.getElementById('list-3').addEventListener('mouseover',function () {
+document.getElementById('list-3').addEventListener('mouseover', function () {
     document.getElementById('line-3').classList.remove('hidden');
 });
 document.getElementById('list-3').addEventListener('mouseout', function () {
@@ -20,16 +20,16 @@ document.getElementById('list-3').addEventListener('mouseout', function () {
 document.getElementById('menu-icon').addEventListener('click', function () {
     document.getElementById('open-menu').classList.remove('hidden');
     document.getElementById('menu-list').classList.remove('hidden');
-/*
-    document.getElementById('moon-line').classList.add('hidden');
-*/
+    /*
+        document.getElementById('moon-line').classList.add('hidden');
+    */
 });
 document.getElementById('close-menu').addEventListener('click', function () {
     document.getElementById('open-menu').classList.add('hidden');
     document.getElementById('menu-list').classList.add('hidden');
-/*
-    document.getElementById('moon-line').classList.remove('hidden');
-*/
+    /*
+        document.getElementById('moon-line').classList.remove('hidden');
+    */
 });
 document.querySelector('[data-foo="2"]').addEventListener('mouseover', function () {
     document.querySelector('#europa-line').classList.remove('hidden');
@@ -56,48 +56,53 @@ document.querySelector('[data-foo="3"]').addEventListener('mouseout', function (
     document.querySelector('[data-foo="3"]').classList.remove('text-gray-400');
 });
 var testresponse;
+
 function loadXMLDoc(planet, li) {
     var xmlhttp = new XMLHttpRequest();
 
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
-           if (xmlhttp.status == 200) {
-               document.querySelectorAll('.planets-title').forEach(function (m){
-                  m.classList.remove('text-white');
-                  m.classList.remove('border-b-2');
-               });
-               li.classList.add('text-white');
-               li.classList.add('border-b-2');
+            if (xmlhttp.status == 200) {
+                document.querySelectorAll('.planets-title').forEach(function (m) {
+                    m.classList.remove('text-white');
+                    m.classList.remove('border-b-2');
+                });
+                li.classList.add('text-white');
+                li.classList.add('border-b-2');
                 plantsMenu(JSON.parse(xmlhttp.responseText));
-           }
-           else if (xmlhttp.status == 400) {
-              alert('There was an error 400');
-           }
-           else {
-               alert('something else other than 200 was returned');
-           }
+            } else if (xmlhttp.status == 400) {
+                alert('There was an error 400');
+            } else {
+                alert('something else other than 200 was returned');
+            }
         }
     };
 
-    xmlhttp.open("GET", "/space-tourism-website-main/starter-code/"+ planet + ".json", true);
-    xmlhttp.open("GET", "/"+ planet + ".json", true);
-xmlhttp.send();
+    xmlhttp.open("GET", "/space-tourism-website-main/starter-code/" + planet + ".json", true);
+        xmlhttp.open("GET", "/"+ planet + ".json", true);
+    xmlhttp.send();
 }
+
 loadXMLDoc('moon', document.getElementById('home'));
- function plantsMenu(response) {
-    document.getElementById('planet').innerHTML= response.planet;
-    document.getElementById('planet-description').innerHTML= response.description;
-    document.getElementById('planet-distance').innerHTML= response.distance;
-    document.getElementById('planet-time').innerHTML= response.time;
+
+function plantsMenu(response) {
+    document.getElementById('planet').innerHTML = response.planet;
+    document.getElementById('planet-description').innerHTML = response.description;
+    document.getElementById('planet-distance').innerHTML = response.distance;
+    document.getElementById('planet-time').innerHTML = response.time;
     document.getElementById('planet-image').src = response.image;
- }
- function loadingContent() {
-     if (document.getElementById('planet-image').complete){
-         document.getElementById('loading').classList.add('hidden');
-         console.log('add');
-     }else {
-         document.getElementById('loading').classList.remove('hidden');
-         console.log('remove');
-     }
- }
- loadingContent();
+}
+
+function loadingContent() {
+    if (document.getElementById('planet-image').complete) {
+        document.getElementById('loading').classList.add('hidden');
+            document.getElementById('moon-content').classList.remove('hidden');
+}
+}
+var body = document.querySelector('body');
+function test() {
+    if (loadingContent() != true) {
+        document.getElementById('loading').classList.remove('hidden');
+        document.getElementById('moon-content').classList.add('hidden');
+    }}
+test();
