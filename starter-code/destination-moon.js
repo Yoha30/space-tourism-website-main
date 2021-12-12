a1a921509ef6680d0a2aca768a0df7e29dffda51
Yoha30 <3202114888@alexu.edu.eg>
2021-12-12 15:25:20 +0200

    home-page

--- starter-code/destination-moon.js
+++ starter-code/destination-moon.js
@@ -1,63 +1,7 @@
-document.getElementById('list').addEventListener('mouseover', function () {
-    document.getElementById('line').classList.remove('hidden');
-});
-document.getElementById('list').addEventListener('mouseout', function () {
-    document.getElementById('line').classList.add('hidden');
-});
-document.getElementById('list-2').addEventListener('mouseover', function () {
-    document.getElementById('line-2').classList.remove('hidden');
-});
-document.getElementById('list-2').addEventListener('mouseout', function () {
-    document.getElementById('line-2').classList.add('hidden');
-});
-document.getElementById('list-3').addEventListener('mouseover', function () {
-    document.getElementById('line-3').classList.remove('hidden');
-});
-document.getElementById('list-3').addEventListener('mouseout', function () {
-    document.getElementById('line-3').classList.add('hidden');
-});
-
-document.getElementById('menu-icon').addEventListener('click', function () {
-    document.getElementById('open-menu').classList.remove('hidden');
-    document.getElementById('menu-list').classList.remove('hidden');
-    /*
-        document.getElementById('moon-line').classList.add('hidden');
-    */
-});
-document.getElementById('close-menu').addEventListener('click', function () {
-    document.getElementById('open-menu').classList.add('hidden');
-    document.getElementById('menu-list').classList.add('hidden');
-    /*
-        document.getElementById('moon-line').classList.remove('hidden');
-    */
-});
-document.querySelector('[data-foo="2"]').addEventListener('mouseover', function () {
-    document.querySelector('#europa-line').classList.remove('hidden');
-    document.querySelector('[data-foo="2"]').classList.add('text-gray-400');
-});
-document.querySelector('[data-foo="2"]').addEventListener('mouseout', function () {
-    document.querySelector('#europa-line').classList.add('hidden');
-    document.querySelector('[data-foo="2"]').classList.remove('text-gray-400');
-});
-document.querySelector('[data-foo="1"]').addEventListener('mouseover', function () {
-    document.querySelector('#mars-line').classList.remove('hidden');
-    document.querySelector('[data-foo="1"]').classList.add('text-gray-400');
-});
-document.querySelector('[data-foo="1"]').addEventListener('mouseout', function () {
-    document.querySelector('#mars-line').classList.add('hidden');
-    document.querySelector('[data-foo="1"]').classList.remove('text-gray-400');
-});
-document.querySelector('[data-foo="3"]').addEventListener('mouseover', function () {
-    document.querySelector('#titan-line').classList.remove('hidden');
-    document.querySelector('[data-foo="3"]').classList.add('text-gray-400');
-});
-document.querySelector('[data-foo="3"]').addEventListener('mouseout', function () {
-    document.querySelector('#titan-line').classList.add('hidden');
-    document.querySelector('[data-foo="3"]').classList.remove('text-gray-400');
-});
 var testresponse;
 
 function loadXMLDoc(planet, li) {
+    document.getElementById('loading').classList.remove('hidden');
     var xmlhttp = new XMLHttpRequest();
 
     xmlhttp.onreadystatechange = function () {
@@ -69,6 +13,7 @@ function loadXMLDoc(planet, li) {
                 });
                 li.classList.add('text-white');
                 li.classList.add('active');
+                loadingContent();
                 plantsMenu(JSON.parse(xmlhttp.responseText));
             } else if (xmlhttp.status == 400) {
                 alert('There was an error 400');
@@ -92,16 +37,15 @@ function plantsMenu(response) {
     document.getElementById('planet-time').innerHTML = response.time;
     document.getElementById('planet-image').src = response.image;
 }
-
+var refreshIntervalId;
 function loadingContent() {
-    if (document.getElementById('planet-image').complete) {
+    refreshIntervalId = setInterval(intervalImages, 500);
+function intervalImages(){
+    console.log(document.getElementById('planet-image').complete);
+            clearInterval(refreshIntervalId);
+    if(document.getElementById('planet-image').complete){
         document.getElementById('loading').classList.add('hidden');
-            document.getElementById('moon-content').classList.remove('hidden');
+        document.getElementById('planet-image').complete = false;
+    }
 }
 }
-function test() {
-    if (loadingContent() != true) {
-        document.getElementById('loading').classList.remove('hidden');
-        document.getElementById('moon-content').classList.add('hidden');
-    }}
-test();
